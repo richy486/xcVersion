f3519c4da8263daed7be12506922aff552162af9
richy <richy486@gmail.com>
2011-11-23 12:53:32 +1100

    First Commit

---
+++ Version.h
@@ -0,0 +1,15 @@
+//
+//  Version.h
+//
+//  Created by Richard Adem on 18/10/11.
+//  Copyright (c) 2011 Richard Adem. All rights reserved.
+//
+// richy486@gmail.com
+// twitter.com/richy486
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Version : UIView
+- (id) initOnView:(UIView*) superView;
+@end
